e01ed2475fd372129ec78a9831d2d6ed91f0593c
Thommy <trob745@aucklanduni.ac.nz>
2015-10-15 19:14:45 +1300

    Added Household class which is a subclass of MixingGroup.

---
+++ src/model/Community.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.*;
+
+public class Community extends MixingGroup {
+	
+	MixingGroup highschool;
+	
+	public Community(int type, double prob) {
+		super(type,prob);
+	}
+	
+}
